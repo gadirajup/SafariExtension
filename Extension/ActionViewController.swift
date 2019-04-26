a926db749ccaff48f3c751ef17b460460b06d450
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-25 22:18:54 -0700

    -extensions

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Prudhvi Gadiraju on 4/25/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypeIdentifierKey as String, options: nil) { [unowned self] (dict, error) in
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    print(javaScriptValues)
+                }
+            }
+        }
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+}
